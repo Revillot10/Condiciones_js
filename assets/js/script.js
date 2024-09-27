8b0c65c514c97071c4b7bfb692971e55a4fa9dca
David Revillot <drevillotna@gmail.com>
2024-09-26 21:13:54 -0300

    second commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,66 @@
-var cantidadProducto = 1; // Inicializa la cantidad en 0
-var precio = 35990;
-
-
-function actualizar() {
-    var totalGeneral = cantidadProducto * precio;
-    document.querySelector('#total').innerHTML = totalGeneral;
-    document.querySelector('#cantidad').innerHTML = cantidadProducto;
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-    actualizar(); // Inicializa los valores al cargar la página
-    
-    var botonSumar = document.querySelector("#sumar");
-    var botonRestar = document.querySelector("#restar");
-    var botonComprar = document.querySelector("#btn-comprar");
-    
-    botonSumar.addEventListener('click', function() {
-        cantidadProducto = cantidadProducto + 1; 
-        actualizar(); 
-    });
-
-    botonRestar.addEventListener('click', function() {
-        if (cantidadProducto > 0) {
-            cantidadProducto = cantidadProducto - 1; 
-            actualizar(); 
+document.addEventListener("click", (e) => {
+    if (e.target.id == "sticker-logo") {
+        const borderImg = document.getElementById("sticker-logo");
+        
+        if (borderImg.style.border == "2px solid red") {
+            borderImg.style.border = "none"; 
+        } else {
+            borderImg.style.border = "2px solid red"; 
         }
-    });
+    }
+    
+    if (e.target.id == "btn-verificar") {
+        const num1 = document.querySelector("#card-1");
+        const num2 = document.querySelector("#card-2");
+        const num3 = document.querySelector("#card-3");
+        
+        if (validarInput(num1) && validarInput(num2) && validarInput(num3)) {
+            const suma = Number(num1.value) + Number(num2.value) + Number(num3.value);
+            if (suma > 10) {
+                document.querySelector("#txt-verificar").innerHTML = "Llevas demasiados stickers!";
+            } else {
+                document.querySelector("#txt-verificar").innerHTML = `Llevas ${suma} stickers.`;
+            }           
+        } else {
+            document.querySelector("#txt-verificar").innerHTML = "";
+        }
+    }
 
-    botonComprar.addEventListener('click', function() {
-        alert("Compra Exitosa!");
-    });
+    // validar los input numéricos
+    function validarInput(inputNum) {
+        let valor = parseFloat(inputNum.value);
 
+        if (Number.isInteger(valor) && valor >= 0) {
+            inputNum.style.border = "1px solid #333"; // borde válido
+            inputNum.style.animation = ""; // reinicio animación
+            inputNum.placeholder = ""; // limpiar placholder
+            return true; // validación exitosa
+        } else {
+            inputNum.style.border = "1px solid red";
+            inputNum.style.animation = "shake-horizontal 0.8s ease-out";
+            inputNum.value = ""; // limpiar el valor del input
+            inputNum.placeholder = "Numero no valido"; // mensaje del placeholder
+            return false; 
+        }
+    }
 
+    // Parte 3 verificación password
+    if (e.target.id == "btn-ingresar") {
+        const digito0 = document.getElementById("digito-0").value;
+        const digito1 = document.getElementById("digito-1").value;
+        const digito2 = document.getElementById("digito-2").value;
+        const password = Number(digito0 + digito1 + digito2); // concatena 
+
+        const mensaje = document.getElementById("txt-password"); // comparar contraseña ingresada con valor correcto
+        password === 911
+            ? (mensaje.innerHTML = "Password 1 correcto") // mensaje correcto
+            : password === 714
+            ? (mensaje.innerHTML = "Password 2 correcto")
+            : (mensaje.innerHTML = "Password Incorrecto!"); // mensaje incorrecto
+    }
 });
+
+
+
 
 
